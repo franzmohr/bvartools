5adedbb6573d4f451eb92ab054216e3b411bfa7c
Franz X. Mohr <franz.x.mohr@outlook.com>
2018-12-08 18:17:29 +0100

    Add E6 data from Luetkepohl (2007)

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' West German economic time series
 #'
-#' The dataset contains quarterly time series for West German fixed investment, disposable
+#' The dataset contains quarterly, seasonally adjusted time series for West German fixed investment, disposable
 #' income, and consumption expenditures in billions of DM from 1960Q1 to 1982Q4. It was produced
 #' from file E1 of the datasets associated with Lütkepohl (2007). Raw data are available at
 #' \url{http://www.jmulti.de/download/datasets/e1.dat} and were originally obtained from
@@ -20,3 +20,27 @@
 #' Lütkepohl, H. (2007). \emph{New introduction to multiple time series analysis}. Berlin: Springer.
 #' 
 "e1"
+
+#' German interest and inflation rate data
+#'
+#' The dataset contains quarterly, seasonally unadjusted time series for German long-term interest
+#' and inflation rates from 1972Q2 to 1998Q4. It was produced from file E6 of the datasets associated with Lütkepohl (2007). Raw
+#' data are available at \url{http://www.jmulti.de/download/datasets/e6.dat} and were originally obtained from
+#' Deutsche Bundesbank and Deutsches Institut für Wirtschaftsforschung.
+#' 
+#' @usage data("e6")
+#' 
+#' @format A named time-series object with 107 rows and 2 variables:
+#' \describe{
+#'   \item{R}{nominal long-term interest rate (Umlaufsrendite).}
+#'   \item{Dp}{\eqn{\Delta} log of GDP deflator.}
+#' }
+#' 
+#' @details The data cover West Germany until 1990Q2 and all of Germany aferwards. The values refer to 
+#' the last month of a quarter.
+#' 
+#' @references
+#' 
+#' Lütkepohl, H. (2007). \emph{New introduction to multiple time series analysis}. Berlin: Springer.
+#' 
+"e6"
--- data-raw/luetkepohl_data.R
+++ data-raw/luetkepohl_data.R
@@ -2,3 +2,8 @@ rm(list = ls())
 
 e1 <- ts(read.table("http://www.jmulti.de/download/datasets/e1.dat", skip = 6, header = TRUE), start = 1960, frequency = 4)
 usethis::use_data(e1, overwrite = TRUE)
+
+
+e6 <- ts(read.table("http://www.jmulti.de/download/datasets/e6.dat", skip = 10, header = TRUE), start = 1972.25, frequency = 4)
+e6 <- e6[, c("R", "Dp")]
+usethis::use_data(e6, overwrite = TRUE)

9306ebb5ccee3209ea19648085bdc404da41cbbc
Franz X. Mohr <franz.x.mohr@outlook.com>
2018-12-18 21:29:40 +0100

    data-raw update

--- data-raw/luetkepohl_data.R
+++ data-raw/luetkepohl_data.R
@@ -1,9 +1,14 @@
 rm(list = ls())
 
-e1 <- ts(read.table("http://www.jmulti.de/download/datasets/e1.dat", skip = 6, header = TRUE), start = 1960, frequency = 4)
+download.file("http://www.jmulti.de/download/datasets/e1.dat",
+              destfile = "data-raw/e1.dat")
+e1 <- ts(read.table("data-raw/e1.dat", skip = 6, header = TRUE),
+         start = 1960, frequency = 4)
 usethis::use_data(e1, overwrite = TRUE)
 
-
-e6 <- ts(read.table("http://www.jmulti.de/download/datasets/e6.dat", skip = 10, header = TRUE), start = 1972.25, frequency = 4)
+download.file("http://www.jmulti.de/download/datasets/e6.dat",
+              destfile = "data-raw/e6.dat")
+e6 <- ts(read.table("data-raw/e6.dat", skip = 10, header = TRUE),
+         start = 1972.25, frequency = 4)
 e6 <- e6[, c("R", "Dp")]
 usethis::use_data(e6, overwrite = TRUE)

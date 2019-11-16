1470e9bc86756d335369972f798725ba4cd3e3a8
Franz X. Mohr <franz.x.mohr@outlook.com>
2019-11-16 16:08:19 +0100

    Add inclusion_prior

--- R/inclusion_prior.R
+++ R/inclusion_prior.R
@@ -6,16 +6,17 @@
 #' @param object an object of class \code{"bvarmodel"}, usually, a result of a call to \code{\link{gen_var}}
 #' or \code{\link{gen_vec}}.
 #' @param prob a numeric specifying the prior inclusion probability of all model parameters.
+#' @param exclude_deterministics logical. If \code{TRUE} (default), the vector of the positions of
+#' included variables does not include the positions of deterministic terms.
 #' @param minnesota_like logical. If \code{TRUE}, the prior inclusion probabilities of the
 #' parameters are calculated in a similar way as the Minnesota prior. See 'Details'.
 #' @param kappa a numeric vector of four elements containing the prior inclusion probabilities
 #' of coefficients that correspond to own lags of endogenous variables, to endogenous variables,
 #' which do not correspond to own lags, to exogenous variables and deterministic terms, respectively.
 #' Only used if \code{minnesota_like = TRUE}. See 'Details'.
-#' @param exclude_deterministics logical. If \code{TRUE} (default), the vector of variables, which should
-#' be included in the BVS algorithm does not include deterministic terms.
 #' 
-#' @details Prior inclusion probabilities \eqn{\underline{\pi}_1} are calculated as
+#' @details If \code{minnesota_like = TRUE}, prior inclusion probabilities \eqn{\underline{\pi}_1}
+#' are calculated as
 #' \tabular{cl}{
 #' \eqn{\frac{\kappa_1}{r}} \tab for own lags of endogenous variables, \cr
 #' \eqn{\frac{\kappa_2}{r}} \tab for other endogenous variables, \cr
@@ -23,7 +24,17 @@
 #' \eqn{\kappa_{4}} \tab for deterministic variables, 
 #' }
 #' for lag \eqn{r} with \eqn{\kappa_1}, \eqn{\kappa_2}, \eqn{\kappa_3}, \eqn{\kappa_4} as the first, second,
-#' thrid and forth element in \code{kappa}, respectively.
+#' third and forth element in \code{kappa}, respectively.
+#' 
+#' For vector error correction models the function generates prior inclusion probabilities for differenced
+#' variables and unrestricted deterministc terms as described above. For variables in the error
+#' correction term prior inclusion probabilites are calculated as
+#' \tabular{cl}{
+#' \eqn{\kappa_1} \tab fow own levels of endogenous variables, \cr
+#' \eqn{\kappa_2} \tab for levels of other endogenous variables, \cr
+#' \eqn{\kappa_3} \tab for levels of exogenous variables, \cr
+#' \eqn{\kappa_4} \tab for deterministic variables.
+#' }
 #' 
 #' @return A list containing a matrix of prior inclusion probabilities and an integer vector
 #' specifying the positions of variables, which should be included in the variable selction algorithm.
@@ -50,14 +61,18 @@
 #' pi_prior <- inclusion_prior(object)
 #' 
 #' @export
-inclusion_prior <- function(object, prob = .5, minnesota_like = FALSE,
-                      kappa = c(0.8, 0.5, 0.5, .8), exclude_deterministics = TRUE) {
+inclusion_prior <- function(object, prob = .5, exclude_deterministics = TRUE,
+                            minnesota_like = FALSE, kappa = c(0.8, 0.5, 0.5, .8)) {
   
-  if (prob > 1 | prob < 0) {
-    stop("Argument 'prob' must be between 0 and 1.")
+  if (!minnesota_like) {
+    if (prob > 1 | prob < 0) {
+      stop("Argument 'prob' must be between 0 and 1.")
+    } 
   }
-  if (any(kappa > 1) | any(kappa < 0)) {
-    stop("Argument 'kappa' may only contain values between 0 and 1.")
+  if (minnesota_like) {
+    if (any(kappa > 1) | any(kappa < 0)) {
+      stop("Argument 'kappa' may only contain values between 0 and 1.")
+    } 
   }
   
   y <- object$Y
@@ -66,8 +81,10 @@ inclusion_prior <- function(object, prob = .5, minnesota_like = FALSE,
   k <- length(object$model$endogenous$variables)
   m <- 0
   n <- 0
+  n_res <- 0
   p <- object$model$endogenous$lags
   s <- 0
+  n_pi <- 0
   
   if (!is.null(object$model$exogen)) {
     m <- length(object$model$exogen$variables)
@@ -87,39 +104,57 @@ inclusion_prior <- function(object, prob = .5, minnesota_like = FALSE,
     } else {
       x <- object$W
     }
+    if (!is.null(object$model$deterministic$restricted)) {
+      n_res <- length(object$model$deterministic$restricted)
+    }
     if (!is.null(object$model$deterministic$unrestricted)) {
       n <- n + length(object$model$deterministic$unrestricted)
     }
     p <- p - 1
     s <- s - 1
+    n_pi <- k + m + n_res
   }
   
   result <- matrix(NA, k, NROW(x))
   include <- matrix(1:(k * NROW(x)))
   
   if (minnesota_like) {
+    if (object$model$type == "VEC") {
+      result[, 1:k] <- kappa[2]
+      if (k > 1) {
+        diag(result[, 1:k]) <- kappa[1] 
+      } else {
+        result[, 1] <- kappa[1]
+      }
+      if (m > 0) {
+        result[, k + 1:m] <- kappa[3]
+      }
+      if (n_res > 0) {
+        result[, k + m + 1:n_res] <- kappa[4]
+      }
+    }
     if (p > 0) {
       for (i in 1:p) {
-        result[, (i - 1) * k + 1:k] <- kappa[2] / i
+        result[, n_pi + (i - 1) * k + 1:k] <- kappa[2] / i
         if (k > 1) {
-          diag(result[, (i - 1) * k + 1:k]) <- kappa[1] / i 
+          diag(result[, n_pi + (i - 1) * k + 1:k]) <- kappa[1] / i 
         } else {
-          result[1, (i - 1) * k + 1:k] <- kappa[1] / i
+          result[, n_pi + (i - 1) * k + 1] <- kappa[1] / i
         }
       }
     }
     
     if (m > 0) {
-      result[, p * k + 1:m] <- kappa[3]
+      result[, n_pi + p * k + 1:m] <- kappa[3]
       if (s > 0) {
         for (i in 1:s) {
-          result[, p * k + m + (i - 1) * k + 1:k] <- kappa[3] / (1 + i)
+          result[, n_pi + p * k + m + (i - 1) * k + 1:k] <- kappa[3] / (1 + i)
         }
       }
     }
     
     if (n > 0) {
-      result[, p * k + (s + 1) * m + 1:n] <- kappa[4]
+      result[, n_pi + p * k + (s + 1) * m + 1:n] <- kappa[4]
     }
   } else {
     result[,] <- prob
@@ -127,8 +162,19 @@ inclusion_prior <- function(object, prob = .5, minnesota_like = FALSE,
   
   result <- matrix(result)
   
-  if (n > 0 & exclude_deterministics) {
-    pos_det <- k * (k * p + (s + 1) * m) + 1:(k * n)
+  if ((n > 0 | n_res > 0) & exclude_deterministics) {
+    if (object$model$type == "VAR") {
+      pos_det <- k * (k * p + (s + 1) * m) + 1:(k * n) 
+    }
+    if (object$model$type == "VEC") {
+      pos_det <- NULL
+      if (n_res > 0) {
+        pos_det <- c(pos_det, k * (k + m) + 1:(k * n_res))
+      }
+      if (n > 0) {
+        pos_det <- c(pos_det, k * (n_pi + k * p + (s + 1) * m) + 1:(k * n))
+      }
+    }
     include <- matrix(include[-pos_det, ])
     if (NROW(include) == 0) {
       warning("Deterministic terms are excluded from BVS and no further parameters are estimated.")

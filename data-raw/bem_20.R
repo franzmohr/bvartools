2a4522c84e8ee7cc48595d77e59e858b456daa60
acer-fxm <franz.x.mohr@outlook.com>
2020-06-18 23:22:56 +0200

    Change US data to smaller time series

--- data-raw/bem_20.R
+++ data-raw/bem_20.R
@@ -1,11 +1,13 @@
 rm(list = ls())
 
 # Specify URL
-#url <- "https://web.ics.purdue.edu/~jltobias/second_edition/Chapter20/code_for_exercise_1/US_macrodata.csv"
-url <- "https://web.ics.purdue.edu/~jltobias/second_edition/Chapter20/code_for_exercise_7/US_macrodata1.csv"
+url <- "https://web.ics.purdue.edu/~jltobias/second_edition/Chapter20/code_for_exercise_1/US_macrodata.csv"
+#url <- "https://web.ics.purdue.edu/~jltobias/second_edition/Chapter20/code_for_exercise_7/US_macrodata1.csv"
 # Load data
-download.file(url, destfile = "data-raw/US_macrodata1.csv")
-data <- read.csv("data-raw/US_macrodata1.csv", col.names = c("date", "Dp", "u", "r"))
+temp <- tempfile()
+download.file(url, destfile = temp)
+data <- read.csv(temp, col.names = c("date", "Dp", "u", "r"))
+unlink(temp)
 # Omit NA values
 data <- na.omit(data)
 # Transform into time-series object

f904b82ed24259c8c09bbfad56ff7029b1e82920
Franz Mohr <franz.x.mohr@outlook.com>
2024-07-24 21:12:15 +0200

    adapt to new initial values function

--- R/bvarpost.R
+++ R/bvarpost.R
@@ -38,7 +38,7 @@
 #' 
 #' @examples
 #' 
-#' # Get data
+#' # Load data
 #' data("e1")
 #' e1 <- diff(log(e1)) * 100
 #' 
@@ -60,67 +60,10 @@
 bvarpost <- function(object) {
   
   if (object[["model"]][["tvp"]]) {
-    object <- .bvartvpalg(object) # Use C++ code to draw posteriors
+    object <- .bvartvpalg(object)
   } else {
     object <- .bvaralg(object)
   }
 
-  if (!is.null(object[["posteriors"]][["sigma"]][["lambda"]])) {
-    k <- NCOL(object[["data"]][["Y"]])
-    sigma_lambda <- matrix(diag(NA_real_, k), k * k, object[["model"]][["iterations"]])
-    sigma_lambda[which(lower.tri(diag(1, k))), ] <- object[["posteriors"]][["sigma"]][["lambda"]]
-    sigma_lambda[which(upper.tri(diag(1, k))), ] <- object[["posteriors"]][["sigma"]][["lambda"]]
-    object[["posteriors"]][["sigma"]][["lambda"]] <- sigma_lambda
-    rm(sigma_lambda)
-  }
-
-  A0 <- NULL
-  if (object[["model"]][["structural"]]) {
-
-    k <- NCOL(object[["data"]][["Y"]])
-    tt <- NROW(object[["data"]][["Y"]])
-    pos <- which(lower.tri(diag(1, k)))
-    draws <- object[["model"]][["iterations"]]
-
-    if (is.list(object[["posteriors"]][["a0"]])) {
-
-      if ("coeffs" %in% names(object[["posteriors"]][["a0"]])) {
-        if (object[["model"]][["tvp"]]) {
-          A0[["coeffs"]] <- matrix(diag(1, k), k * k * tt, draws)
-          A0[["coeffs"]][rep(0:(tt - 1) * k * k, each = length(pos)) + rep(pos, tt), ] <- object[["posteriors"]][["a0"]][["coeffs"]]
-        } else {
-          A0[["coeffs"]] <- matrix(diag(1, k), k * k, draws)
-          A0[["coeffs"]][rep(0:(draws - 1) * k * k, each = length(pos)) + pos ] <- object[["posteriors"]][["a0"]][["coeffs"]]
-        }
-      }
-
-      if ("sigma" %in% names(object[["posteriors"]][["a0"]])) {
-        A0[["sigma"]] <- matrix(0, k * k, draws)
-        A0[["sigma"]][pos, ] <- object[["posteriors"]][["a0"]][["sigma"]]
-      }
-
-      if ("lambda" %in% names(object[["posteriors"]][["a0"]])) {
-        A0[["lambda"]] <- matrix(diag(1, k), k * k, draws)
-        A0[["lambda"]][pos, ] <- object[["posteriors"]][["a0"]][["lambda"]]
-        A0[["lambda"]][-pos, ] <- NA_real_
-      }
-
-    } else {
-      A0 <- matrix(diag(1, k), k * k, draws)
-      A0[pos, ] <- object[["posteriors"]][["a0"]]
-    }
-  }
-
-  # Create bvar object
-  object <- bvar(data = NULL,
-                 exogen = NULL,
-                 y = object[["data"]][["Y"]],
-                 x = object[["data"]][["Z"]],
-                 A0 = A0,
-                 A = object[["posteriors"]][["a"]],
-                 B = object[["posteriors"]][["b"]],
-                 C = object[["posteriors"]][["c"]],
-                 Sigma = object[["posteriors"]][["sigma"]])
-  
   return(object)
 }

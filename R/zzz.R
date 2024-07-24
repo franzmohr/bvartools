a3c20608f4bdfaa4fb7730ec2e556a2668ba5093
Franz Mohr <franz.x.mohr@outlook.com>
2024-07-24 21:09:31 +0200

    move import of Matrix functions to file bvartools-package

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,3 @@
-# Load needed functions from different packages for fast access
-
-#' @importFrom Matrix chol
-#' @importFrom Matrix crossprod
-#' @importFrom Matrix solve
-#' @importFrom Matrix t
 
 # Unload the DLL when the package is unloaded
 .onUnload <- function (libpath) {

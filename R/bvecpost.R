8fd441b7f33f25b0f8dbbcef407af85d75a12fb5
Franz Mohr <franz.x.mohr@outlook.com>
2024-07-24 21:12:53 +0200

    adapt to new initial values function

--- R/bvecpost.R
+++ R/bvecpost.R
@@ -51,17 +51,20 @@
 #' 
 #' @examples
 #' 
-#' # Get data
+#' # Load data
 #' data("e6")
 #' 
 #' # Create model
-#' model <- gen_vec(e6, p = 4, r = 1,
-#'                  const = "unrestricted", seasonal = "unrestricted",
-#'                  iterations = 100, burnin = 10)
+#' model <- create_vec_model(e6, p = 4, r = 1,
+#'                           const = "unrestricted", seasonal = "unrestricted",
+#'                           iterations = 100, burnin = 10)
 #' # Chosen number of iterations and burnin should be much higher.
 #' 
 #' # Add priors
 #' model <- add_priors(model)
+#' 
+#' # Add initial values
+#' model <- add_initial_values(model)
 #'
 #' # Obtain posterior draws 
 #' object <- bvecpost(model)
@@ -70,134 +73,10 @@
 bvecpost <- function(object) {
   
   if (object[["model"]][["tvp"]]) {
-    object <- .bvectvpalg(object) # Use C++ code to draw posteriors
+    object <- .bvectvpalg(object)
   } else {
     object <- .bvecalg(object)
   }
-  
-  k <- NCOL(object[["data"]][["Y"]])
-  p <- object[["model"]][["endogen"]][["lags"]]
-  s <- 0
-  tt <- NROW(object[["data"]][["Y"]])
-  r <- object[["model"]][["rank"]]
-
-  if (!is.null(object[["posteriors"]][["sigma"]][["lambda"]])) {
-    sigma_lambda <- matrix(diag(NA_real_, k), k * k, object[["model"]][["iterations"]])
-    sigma_lambda[which(lower.tri(diag(1, k))), ] <- object[["posteriors"]][["sigma"]][["lambda"]]
-    sigma_lambda[which(upper.tri(diag(1, k))), ] <- object[["posteriors"]][["sigma"]][["lambda"]]
-    object[["posteriors"]][["sigma"]][["lambda"]] <- sigma_lambda
-    rm(sigma_lambda)
-  }
-
-  A0 <- NULL
-  if (object[["model"]][["structural"]]) {
-
-    pos <- which(lower.tri(diag(1, k)))
-    draws <- object[["model"]][["iterations"]]
-
-    if (is.list(object$posteriors[["a0"]])) {
-
-      if ("coeffs" %in% names(object[["posteriors"]][["a0"]])) {
-        if (object[["model"]][["tvp"]]) {
-          A0[["coeffs"]] <- matrix(diag(1, k), k * k * tt, object[["model"]][["iterations"]])
-          A0[["coeffs"]][rep(0:(tt - 1) * k * k, each = length(pos)) + rep(pos, tt), ] <- object[["posteriors"]][["a0"]][["coeffs"]]
-        } else {
-          A0[["coeffs"]] <- matrix(diag(1, k), k * k, object[["model"]][["iterations"]])
-          A0[["coeffs"]][rep(0:(draws - 1) * k * k, each = length(pos)) + pos ] <- object[["posteriors"]][["a0"]][["coeffs"]]
-        }
-      }
-
-      if ("sigma" %in% names(object[["posteriors"]][["a0"]])) {
-        A0[["sigma"]] <- matrix(0, k * k, object[["model"]][["iterations"]])
-        A0[["sigma"]][pos, ] <- object[["posteriors"]][["a0"]][["sigma"]]
-      }
-
-      if ("lambda" %in% names(object[["posteriors"]][["a0"]])) {
-        A0[["lambda"]] <- matrix(diag(1, k), k * k, object[["model"]][["iterations"]])
-        A0[["lambda"]][pos, ] <- object[["posteriors"]][["a0"]][["lambda"]]
-        A0[["lambda"]][-pos, ] <- NA_real_
-      }
-
-    } else {
-      A0 <- matrix(diag(1, k), k * k, object[["model"]][["iterations"]])
-      A0[pos, ] <- object[["posteriors"]][["a0"]]
-    }
-  }
-
-  tsp_temp <- stats::tsp(object[["data"]][["Y"]])
-
-  w <- stats::ts(as.matrix(object[["data"]][["W"]][, 1:k]), class = c("mts", "ts", "matrix"))
-  stats::tsp(w) <- tsp_temp
-  dimnames(w)[[2]] <- dimnames(object[["data"]][["W"]])[[2]][1:k]
-
-  m <- 0
-  if (!is.null(object[["model"]][["exogen"]])) {
-    m <- length(object[["model"]][["exogen"]][["variables"]])
-    w_x <- stats::ts(as.matrix(object[["data"]][["W"]][, k + 1:m]), class = c("mts", "ts", "matrix"))
-    stats::tsp(w_x) <- tsp_temp
-    dimnames(w_x)[[2]] <- dimnames(object[["data"]][["W"]])[[2]][k + 1:m]
-  } else {
-    w_x <- NULL
-  }
-
-  if (!is.null(object[["model"]][["deterministic"]][["restricted"]])) {
-    n_r <- length(object[["model"]][["deterministic"]][["restricted"]])
-    w_d <- stats::ts(as.matrix(object[["data"]][["W"]][, k + m + 1:n_r]), class = c("mts", "ts", "matrix"))
-    stats::tsp(w_d) <- tsp_temp
-    dimnames(w_d)[[2]] <- dimnames(object[["data"]][["W"]])[[2]][k + m + n_r]
-  } else {
-    w_d <- NULL
-  }
-
-  x <- NULL
-  x_x <- NULL
-  x_d <- NULL
-
-  if (!is.null(object[["data"]][["X"]])) {
-
-    if (!is.null(object[["model"]][["endogen"]])) {
-      if (object[["model"]][["endogen"]][["lags"]] > 1) {
-        x <- stats::ts(as.matrix(object[["data"]][["X"]][, 1:(k * (p - 1))]), class = c("mts", "ts", "matrix"))
-        stats::tsp(x) <- tsp_temp
-        dimnames(x)[[2]] <- dimnames(object[["data"]][["X"]])[[2]][1:(k * (p - 1))]
-      }
-    }
-
-    if (!is.null(object[["model"]][["exogen"]])) {
-      s <- object[["model"]][["exogen"]][["lags"]]
-      x_x <- stats::ts(as.matrix(object[["data"]][["X"]][, k * (p - 1) + 1:(m * s)]), class = c("mts", "ts", "matrix"))
-      stats::tsp(x_x) <- tsp_temp
-      dimnames(x_x)[[2]] <- dimnames(object[["data"]][["X"]])[[2]][(k * (p - 1)) + 1:(m * s)]
-    }
-
-    if (!is.null(object[["model"]][["deterministic"]][["unrestricted"]])) {
-      n_ur <- length(object[["model"]][["deterministic"]][["unrestricted"]])
-      x_d <- stats::ts(as.matrix(object[["data"]][["X"]][, k * (p - 1) + m * s + 1:n_ur]), class = c("mts", "ts", "matrix"))
-      stats::tsp(x_d) <- tsp_temp
-      dimnames(x_d)[[2]] <- dimnames(object[["data"]][["X"]])[[2]][k * (p - 1) + m * s + 1:n_ur]
-    }
-  }
-   
-  # Create bvar object
-  object <- bvec(y = object[["data"]][["Y"]],
-                 w = w,
-                 w_x = w_x,
-                 w_d = w_d,
-                 alpha = object[["posteriors"]][["alpha"]],
-                 beta = object[["posteriors"]][["beta"]],
-                 beta_x = object[["posteriors"]][["beta_x"]],
-                 beta_d = object[["posteriors"]][["beta_d"]],
-                 Pi = NULL, Pi_x = NULL, Pi_d = NULL,
-                 x = x,
-                 x_x = x_x,
-                 x_d = x_d,
-                 r = r,
-                 A0 = A0,
-                 Gamma = object[["posteriors"]][["gamma"]],
-                 Upsilon = object[["posteriors"]][["upsilon"]],
-                 C = object[["posteriors"]][["c"]],
-                 Sigma = object[["posteriors"]][["sigma"]],
-                 data = NULL, exogen = NULL)
   
   return(object)
 }

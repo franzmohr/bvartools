ab93167fa2c45fce16e9b0f824cc1d6c80665477
Franz Mohr <franz.x.mohr@outlook.com>
2024-07-24 21:11:22 +0200

    update draw_posterior for bvec

--- R/draw_posterior.bvecmodel.R
+++ R/draw_posterior.bvecmodel.R
@@ -5,18 +5,13 @@
 #' @param object a list of model specifications, which should be passed on
 #' to function \code{FUN}. Usually, the output of a call to \code{\link{gen_vec}}
 #' in combination with \code{\link{add_priors}}.
-#' @param FUN the function to be applied to each list element in argument \code{object}.
+#' @param posterior_function the function to be applied to each list element in argument \code{object}.
 #' If \code{NULL} (default), the internal function \code{\link{bvecpost}} is used.
-#' @param mc.cores the number of cores to use, i.e. at most how many child
-#' processes will be run simultaneously. The option is initialized from
-#' environment variable MC_CORES if set. Must be at least one, and
-#' parallelization requires at least two cores.
 #' @param ... further arguments passed to or from other methods.
 #' 
-#' @return For multiple models a list of objects of class \code{bvarlist}.
-#' For a single model the object has the class of the output of the applied posterior
-#' simulation function. In case the package's own functions are used, this will
-#' be \code{"bvec"}.
+#' @return n object of the class of the output of the applied posterior
+#' simulation function. In case the package's own function is used, this will
+#' result in an object of class \code{"bvec"}.
 #' 
 #' @references
 #' 
@@ -36,61 +31,239 @@
 #' e6 <- e6 * 100
 #' 
 #' # Generate model
-#' model <- gen_vec(e6, p = 1, r = 1, const = "restricted",
-#'                  iterations = 10, burnin = 10)
+#' model <- create_vec_model(e6, p = 1, r = 1, const = "restricted",
+#'                           iterations = 10, burnin = 10)
 #' # Chosen number of iterations and burn-in should be much higher.
 #' 
 #' # Add priors
 #' model <- add_priors(model)
 #' 
+#' # Add initial values
+#' model <- add_initial_values(model)
+#' 
 #' # Obtain posterior draws
 #' object <- draw_posterior(model)
 #' 
 #' @export
-draw_posterior.bvecmodel <- function(object, FUN = NULL, mc.cores = NULL, ...){
+draw_posterior.bvecmodel <- function(object, posterior_function = NULL, ...){
   
-  # Check if it's only one model
-  only_one_model <- FALSE
-  if ("data" %in% names(object)) {
-    object <- list(object)
-    only_one_model <- TRUE
+  model <- object
+  if ("posteriors" %in% names(model)) {
+    model[["posteriors"]] <- NULL
   }
   
-  if (length(object) > 1) {
-    cat("Estimating models...\n")
-  } else {
-    cat("Estimating model...\n") 
-  }
-  
-  if (is.null(mc.cores)) {
-    object <- lapply(object, .posterior_bvecmodel, use = FUN)
-  } else {
-    object <- parallel::mclapply(object, .posterior_bvecmodel, use = FUN,
-                                 mc.cores = mc.cores, mc.preschedule = FALSE)
-  }
-  
-  if (only_one_model) {
-    object <- object[[1]]
-  } else {
-    class(object) <- append("bvarlist", class(object)) 
-  }
-  
-  return(object)
-}
-
-# Helper function to implement try() functionality
-.posterior_bvecmodel <- function(object, use) {
-  
-  if (is.null(use)) {
+  if (is.null(posterior_function)) {
     object <- try(bvecpost(object))
   } else {
     # Apply own function
-    object <- try(use(object))
+    object <- try(posterior_function(object))
   }
   
   # Produce something if estimation fails
   if (inherits(object, "try-error")) {
-    object <- c(object, list(error = TRUE))
+    object <- c(model, list(error = TRUE))
+  } else {
+    
+    if (is.null(posterior_function)) {
+      
+      k <- object[["model"]][["k"]]
+      p <- object[["model"]][["p"]]
+      m <- object[["model"]][["m"]]
+      s <- object[["model"]][["s"]]
+      n_gamma <- k * k * (p - 1)
+      n_upsilon <- k * m * s
+      n_unres <- k * object[["model"]][["n_unrestricted"]]
+      n_a0 <- ifelse(object[["model"]][["structural"]], k * (k - 1) / 2, 0)
+      n_res <- object[["model"]][["n_restricted"]]
+      tt <- NROW(object[["data"]][["y"]])
+      r <- object[["model"]][["rank"]]
+      n_alpha <- r * k
+      n_a <- n_alpha + n_gamma + n_upsilon + n_unres + n_a0
+      pos <- which(lower.tri(diag(1, k)))
+      draws <- object[["model"]][["iterations"]]
+      tvp <- object[["model"]][["tvp"]]
+      
+      # Input data preparation ----
+      
+      tsp_temp <- stats::tsp(object[["data"]][["y"]])
+      
+      w <- stats::ts(as.matrix(object[["data"]][["w"]][, 1:k]), class = c("mts", "ts", "matrix"))
+      stats::tsp(w) <- tsp_temp
+      dimnames(w)[[2]] <- dimnames(object[["data"]][["w"]])[[2]][1:k]
+      
+      if (m > 0) {
+        w_x <- stats::ts(as.matrix(object[["data"]][["w"]][, k + 1:m]), class = c("mts", "ts", "matrix"))
+        stats::tsp(w_x) <- tsp_temp
+        dimnames(w_x)[[2]] <- dimnames(object[["data"]][["w"]])[[2]][k + 1:m]
+      } else {
+        w_x <- NULL
+      }
+      
+      if (n_res > 0) {
+        w_d <- stats::ts(as.matrix(object[["data"]][["w"]][, k + m + 1:n_res]), class = c("mts", "ts", "matrix"))
+        stats::tsp(w_d) <- tsp_temp
+        dimnames(w_d)[[2]] <- dimnames(object[["data"]][["w"]])[[2]][k + m + n_res]
+      } else {
+        w_d <- NULL
+      }
+      
+      x <- NULL
+      x_x <- NULL
+      x_d <- NULL
+      
+      if (!is.null(object[["data"]][["x"]])) {
+        
+        if (p > 1) {
+          x <- stats::ts(as.matrix(object[["data"]][["x"]][, 1:(k * (p - 1))]), class = c("mts", "ts", "matrix"))
+          stats::tsp(x) <- tsp_temp
+          dimnames(x)[[2]] <- dimnames(object[["data"]][["x"]])[[2]][1:(k * (p - 1))]
+        }
+        
+        
+        if (m > 0) {
+          x_x <- stats::ts(as.matrix(object[["data"]][["x"]][, k * (p - 1) + 1:(m * s)]), class = c("mts", "ts", "matrix"))
+          stats::tsp(x_x) <- tsp_temp
+          dimnames(x_x)[[2]] <- dimnames(object[["data"]][["x"]])[[2]][(k * (p - 1)) + 1:(m * s)]
+        }
+        
+        if (object[["model"]][["n_unrestricted"]] > 0) {
+          n_ur <- object[["model"]][["n_unrestricted"]]
+          x_d <- stats::ts(as.matrix(object[["data"]][["x"]][, k * (p - 1) + m * s + 1:n_ur]), class = c("mts", "ts", "matrix"))
+          stats::tsp(x_d) <- tsp_temp
+          dimnames(x_d)[[2]] <- dimnames(object[["data"]][["x"]])[[2]][k * (p - 1) + m * s + 1:n_ur]
+        }
+      }
+      
+      # Coefficient draws ----
+      
+      ## alpha ----
+      alpha <- NULL
+      beta <- NULL
+      beta_x <- NULL
+      beta_d <- NULL
+      n_beta <- r * k
+      n_beta_x <- r * m
+      n_beta_c <- r * n_res
+      k_ect <- k + m + n_res
+      n_beta_tot <- n_beta + n_beta_x + n_beta_c
+      if (r > 0) {
+        
+        pos_alpha <- 1:n_alpha
+        if (tvp) {
+          pos_alpha <- rep(pos_alpha, tt) + rep(0:(tt - 1), each = length(pos_alpha)) * n_a 
+        }
+        alpha <- t(object[["posteriors"]][["a"]][["coeffs"]][, pos_alpha])
+        
+        pos_beta <- rep((1:r - 1) * k_ect, each = k) + rep(1:k, r)
+        if (tvp) {
+          pos_beta <- rep(pos_beta, tt) + rep(0:(tt - 1), each = length(pos_beta)) * n_beta_tot
+        }
+        beta <- t(object[["posteriors"]][["beta"]][["coeffs"]][, pos_beta])
+        
+        if (m > 0) {
+          pos_beta_x <- rep((1:r - 1) * k_ect, each = m) + rep(k + 1:m, r)
+          if (tvp) {
+            pos_beta_x <- rep(pos_beta_x, tt) + rep(0:(tt - 1), each = length(pos_beta_x)) * n_beta_tot
+          }
+          beta_x <- t(object[["posteriors"]][["beta"]][["coeffs"]][, pos_beta_x]) 
+        }
+        
+        if (n_res > 0) {
+          pos_beta_c <- rep((1:r - 1) * k_ect, each = n_res) + rep(k + m + 1:n_res, r)
+          if (tvp) {
+            pos_beta_c <- rep(pos_beta_c, tt) + rep(0:(tt - 1), each = length(pos_beta_c)) * n_beta_tot
+          }
+          beta_d <- t(object[["posteriors"]][["beta"]][["coeffs"]][, pos_beta_c])
+        }
+      }
+      
+      # Modelled variables ----
+      gamma <- NULL
+      if (p > 1) {
+        pos_gamma <- n_alpha + 1:n_gamma
+        if (tvp) {
+          pos_gamma <- rep(pos_gamma, tt) + rep(0:(tt - 1), each = length(pos_gamma)) * n_a
+        }
+        gamma[["coeffs"]] <- t(object[["posteriors"]][["a"]][["coeffs"]][, pos_gamma])
+        
+        if ("sigma" %in% names(object[["posteriors"]][["a"]])) {
+          gamma[["sigma"]] <- matrix(NA, n_gamma, draws)
+          gamma[["sigma"]] <- t(object[["posteriors"]][["a"]][["sigma"]][, pos_gamma[1:n_gamma]])
+        }
+        
+        if ("lambda" %in% names(object[["posteriors"]][["a"]])) {
+          gamma[["lambda"]] <- matrix(NA, n_gamma, draws)
+          gamma[["lambda"]] <- t(object[["posteriors"]][["a"]][["lambda"]][, pos_gamma[1:n_gamma]])
+        }
+      }
+      
+      # Unmodelled variables ----
+      upsilon <- NULL
+      if (m > 0) {
+        pos_upsilon <- n_alpha + n_gamma + 1:n_upsilon
+        if (tvp) {
+          pos_upsilon <- rep(pos_upsilon, tt) + rep(0:(tt - 1), each = length(pos_upsilon)) * n_a
+        }
+        upsilon[["coeffs"]] <- t(object[["posteriors"]][["a"]][["coeffs"]][, pos_upsilon])
+        
+        if ("sigma" %in% names(object[["posteriors"]][["a"]])) {
+          upsilon[["sigma"]] <- matrix(NA, n_upsilon, draws)
+          upsilon[["sigma"]] <- t(object[["posteriors"]][["a"]][["sigma"]][, pos_upsilon[1:n_upsilon]])
+        }
+        
+        if ("lambda" %in% names(object[["posteriors"]][["a"]])) {
+          upsilon[["lambda"]] <- matrix(NA, n_upsilon, draws)
+          upsilon[["lambda"]] <- t(object[["posteriors"]][["a"]][["lambda"]][, pos_upsilon[1:n_upsilon]])
+        }
+      }
+      
+      # Unrestricted deterministic terms ----
+      c_unres <- NULL
+      if (n_unres > 0) {
+        pos_unres <- n_alpha + n_gamma + n_upsilon + 1:n_unres
+        if (tvp) {
+          pos_unres <- rep(pos_unres, tt) + rep(0:(tt - 1), each = length(pos_unres)) * n_a
+        }
+        c_unres[["coeffs"]] <- t(object[["posteriors"]][["a"]][["coeffs"]][, pos_unres])
+        
+        if ("sigma" %in% names(object[["posteriors"]][["a"]])) {
+          c_unres[["sigma"]] <- matrix(NA, n_unres, draws)
+          c_unres[["sigma"]] <- t(object[["posteriors"]][["a"]][["sigma"]][, pos_unres[1:n_unres]])
+        }
+        
+        if ("lambda" %in% names(object[["posteriors"]][["a"]])) {
+          c_unres[["lambda"]] <- matrix(NA, n_unres, draws)
+          c_unres[["lambda"]] <- t(object[["posteriors"]][["a"]][["lambda"]][, pos_unres[1:n_unres]])
+        }
+      }
+      
+      # Structural ----
+      draws_a0 <- .extract_posterior_a0(object)
+      
+      # Sigma----
+      draws_sigma <- .extract_posterior_sigma(object)
+      
+      # Create bvar object
+      object <- bvec(y = object[["data"]][["y"]],
+                     w = w,
+                     w_x = w_x,
+                     w_d = w_d,
+                     alpha = alpha,
+                     beta = beta,
+                     beta_x = beta_x,
+                     beta_d = beta_d,
+                     Pi = NULL, Pi_x = NULL, Pi_d = NULL,
+                     x = x,
+                     x_x = x_x,
+                     x_d = x_d,
+                     r = r,
+                     A0 = draws_a0,
+                     Gamma = gamma,
+                     Upsilon = upsilon,
+                     C = c_unres,
+                     Sigma = draws_sigma,
+                     data = NULL, exogen = NULL)
+    }
   }
   
   return(object)

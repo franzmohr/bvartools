82c3dbe7c2183550d465bfac0f6b3ac2e4f33ce5
Franz Mohr <franz.x.mohr@outlook.com>
2024-07-25 22:19:49 +0200

    add capability to add horizontal line at zero

--- R/tvpribbon.R
+++ R/tvpribbon.R
@@ -1,6 +1,11 @@
 
 # Use for the extraction of time-varying parameters in plot.bvar()
-.tvpribbon <- function(x, var, ymin, ymax) {
-  draws <- stats::ts(cbind(t(matrix(unlist(lapply(x, function(x, var, ymin, ymax) {stats::quantile(x[, var], probs = c(ymin, .5, ymax))},
-                                           var = var, ymin = ymin, ymax = ymax)), 3)), 0))
+.tvpribbon <- function(x, var, ymin, ymax, show_zero_y) {
+  draws <- stats::ts(t(matrix(unlist(lapply(x, function(x, var, ymin, ymax) {stats::quantile(x[, var], probs = c(ymin, .5, ymax))},
+                                            var = var, ymin = ymin, ymax = ymax)), 3)))
+  
+  if (show_zero_y) {
+    draws <- cbind(draws, 0)
+  }
+  return(draws)
 }

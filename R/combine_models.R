f527f7c12f8cba7a0c35d99792615e99b15a2c98
Franz Mohr <franz.x.mohr@outlook.com>
2026-08-26 22:35:30 +0200

    add functionality to combine model objects

---
+++ R/combine_models.R
@@ -0,0 +1,52 @@
+#' Combine Models
+#' 
+#' Convenience function that combines multiple models into one object.
+#' 
+#' @param ... One or multiple lists that contain either model objects or
+#' posterior objects.
+#' 
+#' 
+#' @examples
+#' 
+#' # Load data
+#' data(e6)
+#' 
+#' # Create multiple VAR models
+#' model_1 <- create_bvarmodel(diff(e6), p = 0:2, deterministic = "const")
+#' 
+#' # Create multiple VEC models
+#' model_2 <- create_bvecmodel(e6, p = 1, r = 0:1, const = "unrestricted")
+#' 
+#' # Combine the models in one object
+#' model <- combine_models(model_1, model_2)
+#' 
+#' @export
+combine_models <- function(...) {
+  
+  input <- list(...)
+  
+  classes <- unlist(lapply(input, class))
+  classes <- classes[-which(classes == "list")]
+  
+  supported_models <- c("bvarmodel", "bvecmodel", "dfmodel", "klgs2010", "modellist",
+                        "expandwindmodellist")
+  supported_total <- supported_models
+  if (!all(classes %in% supported_total)) {
+    stop("The class for at least one input object could not be recognised.")
+  }
+  
+  result <- NULL
+  
+  for (i in 1:length(input)) {
+    if (any(c("bvarmodel", "bvecmodel", "dfmodel", "expandwindmodellist") %in% class(input[[i]]))) {
+      result <- c(result, list(input[[i]]))
+    }
+    if (any(c("modellist") %in% class(input[[i]]))) {
+      result <- c(result, input[[i]])
+    }
+  }
+  class(result) <- append("modellist", class(result))
+  
+  return(result)
+  
+}

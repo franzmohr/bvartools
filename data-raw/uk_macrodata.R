4a64f3a1343cdcf3daa0b23cbded6f52228940f6
Franz Mohr <franz.x.mohr@outlook.com>
2026-08-27 20:47:15 +0200

    clean up data

--- data-raw/uk_macrodata.R
+++ data-raw/uk_macrodata.R
@@ -1,11 +1,9 @@
 rm(list = ls())
 
-uk <- read.delim("~/Dropbox/R/bvartools/data-raw/data_uk.txt", header = FALSE,
-                        col.names = c("date", "u", "r", "Dp"))
-
-uk_macrodata <- ts(uk[, c("Dp", "u", "r")], start = uk[1, 1], frequency = 4)
-
+# Load data
+raw <- read.table("data-raw/uk_macrodata.txt")
+uk_macrodata <- ts(raw[, -1], start = c(1957, 2), frequency = 4)
+dimnames(uk_macrodata) <- list(NULL, c("rs", "rl", "Dp"))
+plot(uk_macrodata)
 
 usethis::use_data(uk_macrodata, overwrite = TRUE)
-
-#plot(uk_macrodata)

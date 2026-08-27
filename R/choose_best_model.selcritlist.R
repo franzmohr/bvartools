009fedf24fc110d3683f574d59000ad46e898b63
Franz Mohr <franz.x.mohr@outlook.com>
2026-08-27 22:58:31 +0200

    add choose_best_model

--- R/choose_best_model.selcritlist.R
+++ R/choose_best_model.selcritlist.R
@@ -22,21 +22,26 @@
 #' e1 <- diff(log(e1)) * 100
 #' 
 #' # Create model
-#' model <- create_var_model(e1, p = 1:3, deterministic = "const",
+#' model <- create_bvarmodel(e1, p = 1:3, deterministic = "const",
 #'                           iterations = 50, burnin = 10)
 #' # Number of iterations and burnin should be much higher.
 #' 
 #' # Add priors
-#' model <- add_priors(model)
+#' model <- add_priors(model,
+#'                     coef = list(v_i = 1, v_i_det = 1 / 10),
+#'                     sigma = list(df = "k", scale = 1))
 #' 
 #' # Add initial values
 #' model <- add_initial_values(model)
 #'
 #' # Obtain posterior draws 
-#' object <- draw_posterior(model)
+#' model <- add_posterior_coefficients(model)
+#' 
+#' # Obtain posterior log-likelihoods
+#' model <- add_posterior_loglik(model)
 #' 
 #' # Calculate selection criteria
-#' sel <- selection_criteria(object)
+#' sel <- selection_criteria(model)
 #' 
 #' # View results
 #' sel
@@ -48,15 +53,16 @@
 #' choose_best_model(sel, criterion = "BIC")
 #' 
 #' @export
+#' @method choose_best_model selcritlist
 choose_best_model.selcritlist <- function(object, criterion = "BIC", ...) {
   
-  res <- lapply(object, function(y) {y[["summary"]]})
+  res <- lapply(object, function(y, criterion) {y[[criterion]][, "mean"]}, criterion = criterion)
   res <- do.call("rbind", res)
   
   if (criterion == "LL") {
-    pos <- which(res[, criterion] == max(res[, criterion]))
+    pos <- which(res == max(res))
   } else {
-    pos <- which(res[, criterion] == min(res[, criterion])) 
+    pos <- which(res == min(res)) 
   }
   
   return(pos)
